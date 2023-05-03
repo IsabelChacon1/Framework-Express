ba9df8327b4fe205b62538a090dd98d321130bdd
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-05-03 09:05:50 -0700

    Correcciones de app

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-var express = require('express');
-const { listen } = require('express/lib/application');
-var app = express();
 
-app.get('/', function(req, res){
-    res.send('Hola mundo!');
-});
-
-app.listen(3000, function(){
-    console.log('Aplicacion de ejemplo, escuchando el puerto 3000!');
-});
-
-//const express = require('express');
+const express = require('express');
 const port = 3002;
 
 const bodyParser= require('body-parser');
 const routes =require('./routes/routes')
-//const app =express();
+const app =express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
