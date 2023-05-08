f8bfe618abdf80e1acd55b3634b621aa2fb449f2
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-05-08 09:47:32 -0700

    Consultas de la tabla productos

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,10 @@ const routes = app =>{
             message:'Bienvenido a Node.js Express REST API!'
         });
     });
-//Mostrar todos los usuarios
+
+    //USUARIOS
+
+    //Mostrar todos los usuarios
 app.get('/users',(request, response)=>{
     pool.query('SELECT * FROM users', (error, result)=>{
         if (error) throw error;
@@ -53,6 +56,55 @@ app.get('/users/:id',(request, response)=>{
         if (error) throw error;
 
         response.send('User deleted successfully. ');
+    });
+});
+
+//PRODUCTOS
+
+//Mostrar todos los productos
+app.get('/productos',(request, response)=>{
+    pool.query('SELECT * FROM productos', (error, result)=>{
+        if (error) throw error;
+
+        response.send(result);
+    });
+});
+
+//Mostrar un solo productos por ID
+app.get('/users',(request, response)=>{
+    pool.query('SELECT * FROM productos where id=?', (error, result)=>{
+        if (error) throw error;
+
+        response.send(result);
+    });
+});
+
+//Agregar un nuevo productos 
+app.get('/productos',(request, response)=>{
+    pool.query('INSERT INTO productos SET ?', request.body, (error, result)=>{
+        if (error) throw error;
+
+        response.send(201).send(`Products added with ID: ${result.insertId}`);
+    });
+});
+
+//Actualizar un usuario existente
+app.get('/productos/:id',(request, response)=>{
+    const id = request.params.id;
+    pool.query('UPDATE productos SET ? WHERE id=?', [request.body, id], (error, result)=>{
+        if (error) throw error;
+
+        response.send('Products updated successfully. ');
+    });
+});
+
+//Eliminar un productos existente
+app.get('/productos/:id',(request, response)=>{
+    const id = request.params.id;
+    pool.query('DELETE FROM productos WHERE id=?', id, (error, result)=>{
+        if (error) throw error;
+
+        response.send('Products deleted successfully. ');
     });
 });
 
