0f23f605a36ad1137b270f5ffbad6dbddccbea45
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-05-08 09:35:58 -0700

    Eliminar un usuario existente

--- routes/routes.js
+++ routes/routes.js
@@ -46,5 +46,15 @@ app.get('/users/:id',(request, response)=>{
     });
 });
 
+//Eliminar un usuario existente
+app.get('/users/:id',(request, response)=>{
+    const id = request.params.id;
+    pool.query('DELETE FROM users WHERE id=?', id, (error, result)=>{
+        if (error) throw error;
+
+        response.send('User deleted successfully. ');
+    });
+});
+
 };
 module.exports = routes;
